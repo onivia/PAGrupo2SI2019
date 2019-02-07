36836e0b8da636035a3e43027604e4c8e87bd8f9
ONivia <ONivia@ONIVIA2019.grasco.com>
2019-02-06 19:17:40 -0500

    mi primer proyecto, protegido en Git.

---
+++ 6pm_06feb2019_1/src/6pm_06feb2019_1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 6pm_06feb2019_1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hola companeros 6pm!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
