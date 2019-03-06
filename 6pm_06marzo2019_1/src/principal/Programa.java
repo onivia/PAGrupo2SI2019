cdab3c9d60046b0b06d0140c68f227705aae444a
ONivia <ONivia@ONIVIA2019.grasco.com>
2019-03-06 18:45:49 -0500

    presendencia de operadores java.

---
+++ 6pm_06marzo2019_1/src/principal/Programa.java
@@ -0,0 +1,23 @@
+package principal;
+
+public class Programa {
+	public static void main(String[] args) {
+		//local, son primtivas o tambn pueden ser apuntadores
+		//static
+		//instance, es un apuntador
+		//las constants
+		//presedencia de operadores
+		//1. () 2. ++, --, 3. */% 4. +-
+		int r = (100 + 50) / 2 - 5 * 3;
+		// 150 / 2 -5 * 3
+		// 150 / 2 - 15
+		// 150 / -13
+		//-11
+		//
+		//100 + 25 - 5 * 3
+		//100 + 25 - 15
+		//125 - 15
+		//110
+		System.out.println(r);
+	}
+}
