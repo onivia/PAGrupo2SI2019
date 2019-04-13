1834a162fc4c6643c98498562ed0f3f3481dc382
ONivia <ONivia@ONIVIA2019.grasco.com>
2019-04-12 19:48:55 -0500

    se completan: hashset hashmap

--- 6pm_10abril2019_1/src/colecciones/Programa.java
+++ 6pm_10abril2019_1/src/colecciones/Programa.java
@@ -1,17 +1,23 @@
 package colecciones;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		pruebaArrayList();
+		//pruebaArrayList();
+		//pruebaHashSet();
+		pruebaHashMap();
 	}
 	
 	public static void pruebaArrayList() {
 		List<Estudiante> arraylist = new ArrayList<Estudiante>();
-		Estudiante e = null;
+		Estudiante e = null;		
 		
 		e = new Estudiante(100, "yo100", "A");
 		arraylist.add(e);
@@ -29,5 +35,30 @@ public class Programa {
 			System.out.println(en);
 		}
 		*/		
+	}
+	
+	public static void pruebaHashSet() {
+		Set<Estudiante> set = new HashSet<Estudiante>();
+		boolean adiciono = false;
+		
+		adiciono = set.add(new Estudiante(100, "yo100", "A"));
+		adiciono = set.add(new Estudiante(200, "yo200", "B"));
+		adiciono = set.add(new Estudiante(100, "yo100", "A"));
+//		System.out.println(adiciono);
+		System.out.println(set.remove(new Estudiante(200, "yo200", "B")));
+		System.out.println(set.size());
+//		for(Estudiante e : set) {
+//			System.out.println(e);
+//		}
+	}
+	
+	public static void pruebaHashMap() {
+		Map<Integer,Estudiante> map = new HashMap<Integer,Estudiante>();
+		map.put(100, new Estudiante(100,"yo100","A"));
+		map.put(200, new Estudiante(200,"yo200","B"));
+		map.put(300, new Estudiante(100,"yo100","Z"));
+		
+		System.out.println(map.size());
+		System.out.println(map.get(300));
 	}
 }
